74a3f5551373719bc0f9f1a80bb5d28c5399ae07
Arne Hassel <arne.hassel@gmail.com>
2015-04-19 21:10:18 +0200

    Fixed touch events

--- src/runs/handleTouchEvents.js
+++ src/runs/handleTouchEvents.js
@@ -3,12 +3,13 @@ import Hammer from 'hammerjs/hammer.js';
 class HandleTouchEvents {
   constructor($rootScope, $state, $window) {
     // touch events
+    delete Hammer.defaults.cssProps.userSelect;
     var mainElement = $window.document.getElementsByTagName('body')[0];
     var hammertime = new Hammer(mainElement);
-    hammertime.on('panleft', function () {
+    hammertime.on('swipeleft', function () {
       $state.current.slide.goNext($state);
     });
-    hammertime.on('panright', function () {
+    hammertime.on('swiperight', function () {
       $state.current.slide.goPrevious($state);
     });
   }
