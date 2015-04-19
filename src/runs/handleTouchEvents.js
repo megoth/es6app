768c7cda2c613b244760dfe0f8f2ff69eed199d8
Arne Hassel <arne.hassel@gmail.com>
2015-04-19 20:09:16 +0200

    Modified touch events, hopefully these work

--- src/runs/handleTouchEvents.js
+++ src/runs/handleTouchEvents.js
@@ -3,12 +3,12 @@ import Hammer from 'hammerjs/hammer.js';
 class HandleTouchEvents {
   constructor($rootScope, $state, $window) {
     // touch events
-    var hammertime = new Hammer($window.document.getElementsByTagName('body')[0]);
-    hammertime.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-    hammertime.on('swipeleft', function () {
+    var mainElement = $window.document.getElementsByTagName('body')[0];
+    var hammertime = new Hammer(mainElement);
+    hammertime.on('panleft', function () {
       $state.current.slide.goNext($state);
     });
-    hammertime.on('swiperight', function () {
+    hammertime.on('panright', function () {
       $state.current.slide.goPrevious($state);
     });
   }
